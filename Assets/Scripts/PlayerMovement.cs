b7bd7892461e1f8668a501bb5de53dd3e3d8b218
ugurrApak <101298562+ugurrApak@users.noreply.github.com>
2023-04-16 21:22:55 +0300

    change jumpforce and speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] float speed = 10f;
+    [SerializeField] LayerMask layer;
+    [SerializeField] bool isGround = false;
+    [SerializeField] float jumpForce = 12f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -14,14 +17,35 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxisRaw("Horizontal");
         PlayerMove(horizontal);
+        GroundCheck();
+        Jump();
     }
     void PlayerMove(float horizontal)
     {
         if (rb != null)
         {
             rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
+    }
+    private void GroundCheck()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, 1.3f,layer);
+        if(ray.collider != null)
+        {
+            isGround= true;
+        }
+        else
+        {
+            isGround= false;
+        }
+    }
+    void Jump()
+    {
+        if (rb != null && isGround)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x,Input.GetAxisRaw("Jump") * jumpForce));
         }
     }
 }
