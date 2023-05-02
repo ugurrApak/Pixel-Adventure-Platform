e2670d9c7a42f93958dab18844244e3d7eec87d9
ugurrApak <101298562+ugurrApak@users.noreply.github.com>
2023-05-02 23:27:53 +0300

    Player-Death-fixed

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -55,6 +55,7 @@ public class UIManager : MonoBehaviour
     {
         Time.timeScale = 1f;
         Ingame = true;
+        Player.IsDead = false;
         SceneLoader.Instance.LoadMainMenu();
     }
     void Pause()
